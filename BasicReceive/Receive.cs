0a5533a7aaebfedff1494e80d6dc98d7dda8d9cb
Shihab Khan <khanshi@hhsc.ca>
2022-01-07 18:08:11 -0500

    Add project files.

---
+++ BasicReceive/Receive.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace BasicReceive
+{
+    class Receive
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                //in consumer we need to declare queue as well, since we might start consumer
+                //before publisher. This will ensure queue exists
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                //provide callback for when message gets pushed to this consumer
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+
+                channel.BasicConsume(queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine("Please enter to exit");
+                Console.ReadLine();
+            }
+
+            
+
+        }
+    }
+}
---
+++ BasicSend/Send.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace BasicSend
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", //will only get created if it doesn't exist already
+                    durable: false,  //should this queue survive following broker restart
+                    exclusive: false,
+                    autoDelete: false, //should this queue be autoDelete if no consumers?
+                    arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                    routingKey: "hello",
+                                    basicProperties: null,
+                                    body: body);
+
+                Console.WriteLine(" [x] Sent {0}", message);
+
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
