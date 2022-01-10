a86ef718bd8d409cb30f4796d6ddd70df3ffcbe9
Shihab Khan <khanshi@hhsc.ca>
2022-01-09 20:31:46 -0500

    implement fanout exchange with temporary queue

---
+++ Consumer/ReceiveLogs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class ReceiveLogs
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+                //this will generate a non-durable, exclusive, autodelete queue with 
+                //a generated name, meaning this will be deleted as soon as the connection closes
+                //or last consumer unsubscribes
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(queue: queueName,
+                                    exchange: "logs",
+                                    routingKey: "");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] {0} ", message);
+                };
+
+                channel.BasicConsume(queue: queueName,
+                    autoAck: true,
+                    consumer: consumer);
+                Console.WriteLine("Press [enter] to exit");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ Publisher/EmitLog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class EmitLog
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //exclusively declare an exchange with fanout (send to all queues)
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "logs",
+                                    routingKey: "", //this value is ignored in fanout exchange
+                                    basicProperties: null,
+                                    body: body);
+
+                Console.WriteLine("[x] Sent {0}", message);
+
+            }
+
+            Console.WriteLine("Please [enter] to exit.");
+            Console.ReadLine();
+
+            //note that if this publisher runs first, all messages will be lost since no queue
+            //is bound yet. Only when a queue is bound to the exchange will the message 
+            //start queuing up
+
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) 
+                ? string.Join(" ", args)
+                : "info: Hello World!");
+        }
+    }
+}
