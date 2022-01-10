1c695bd77cd2aa6359ed3c05dc4a5e42a9025d3f
Shihab Khan <khanshi@hhsc.ca>
2022-01-10 15:17:45 -0500

    implement direct exchange with matching consumer binding key

---
+++ ReceiveLogsDirect/ReceiveLogsDirect.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogsDirect
+{
+    class ReceiveLogsDirect
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs",
+                                        type: "direct");
+
+                //creates generated quename
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if (args.Length < 1)
+                {
+                    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                        Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine("Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+
+                foreach (var severity in args)
+                {
+                    //this is how we bind. In direct exchange, a message goes to queue
+                    //whose binding key exactly matches the routing key of message
+                    //it is also ok to bind multiple queue with same binding key 
+                    channel.QueueBind(queue: queueName,
+                                    exchange: "direct_logs",
+                                    routingKey: severity);  //this is the binding key
+                }
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'",
+                        routingKey, message);
+
+                };
+                channel.BasicConsume(queue: queueName,
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
